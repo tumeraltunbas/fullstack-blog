bd24c9fd5f93f369c4dacbd5c87364c4b22f677f
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-25 15:43:21 +0300

    Add cookieParser

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes/index.js";
+import cookieParser from "cookie-parser";
 import {errorHandler} from "./middlewares/error/errorHandler.js";
 import { databaseConnection } from "./utils/database/databaseConnection.js";
 
@@ -11,6 +12,7 @@ const app = express();
 databaseConnection();
 app.use(cors());
 app.use(express.static("/public"));
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler)
