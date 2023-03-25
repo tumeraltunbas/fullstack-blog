46601807b790d1c139fb8919819d2f28f4aa1ebd
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-25 14:53:40 +0300

    Update config.example.env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import routes from "./routes/index.js";
+import { errorHandler } from "./middlewares/error/errorHandler.js";
 import { databaseConnection } from "./utils/database/databaseConnection.js";
+
 
 dotenv.config({path: "./config/config.env"});
 const app = express();
@@ -10,5 +13,7 @@ databaseConnection();
 app.use(cors());
 app.use(express.static("/public"));
 app.use(express.json());
+app.use("/api", routes);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
