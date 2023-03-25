71483fb7f5ace56b27d47daa248e62fd18aed48a
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-25 14:54:10 +0300

    Revert "Update config.example.env"
    This reverts commit 46601807b790d1c139fb8919819d2f28f4aa1ebd.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import routes from "./routes/index.js";
-import { errorHandler } from "./middlewares/error/errorHandler.js";
 import { databaseConnection } from "./utils/database/databaseConnection.js";
-
 
 dotenv.config({path: "./config/config.env"});
 const app = express();
@@ -13,7 +10,5 @@ databaseConnection();
 app.use(cors());
 app.use(express.static("/public"));
 app.use(express.json());
-app.use("/api", routes);
-app.use(errorHandler);
 
 app.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import authRoutes from "./auth.js";
-
-const router = Router();
-
-router.use("/auth", authRoutes);
-
-export default router;
