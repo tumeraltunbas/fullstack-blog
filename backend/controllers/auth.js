d597ea82c671b6ddc98e3c84808e687645a54470
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-25 17:50:02 +0300

    Add getUser endpoint

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -28,3 +28,9 @@ export const register = async(req, res, next) => {
         return next(err);
     }
 }
+
+export const getUser = async(req, res, next) => {
+    return res
+    .status(200)
+    .json({success:true, user:req.user});
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.js";
+import { register, getUser } from "../controllers/auth.js";
+import {getAccessToRoute} from "../middlewares/auth/auth.js";
+
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/getUser", getAccessToRoute, getUser);
 
 export default router;
