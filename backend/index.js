dfb486136e64d441cee3c5c30a894aee05da169d
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-25 14:57:13 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import routes from "./routes/index.js";
+import {errorHandler} from "./middlewares/error/errorHandler.js";
 import { databaseConnection } from "./utils/database/databaseConnection.js";
 
 dotenv.config({path: "./config/config.env"});
@@ -10,5 +12,7 @@ databaseConnection();
 app.use(cors());
 app.use(express.static("/public"));
 app.use(express.json());
+app.use("/api", routes);
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => console.log(`Server is up at ${process.env.PORT}`));
