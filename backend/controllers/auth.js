dba30a73879a215a51fe53ad4b35ae8847390329
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-25 18:24:31 +0300

    Add login endpoint

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,6 +2,8 @@ import { User } from "../models/User.js";
 import CustomError from "../utils/error/CustomError.js";
 import { checkEmail, checkPassword } from "../utils/helpers/inputHelpers.js";
 import { sendJwtToCookie } from "../utils/helpers/jwtHelpers.js";
+import bcrypt from "bcryptjs";
+
 
 export const register = async(req, res, next) => {
     try{
@@ -23,6 +25,34 @@ export const register = async(req, res, next) => {
 
         sendJwtToCookie(user, res);
         
+    }
+    catch(err){
+        return next(err);
+    }
+}
+
+
+export const login = async(req, res, next) => {
+    
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return next(new CustomError(400, "You have to provide an email and password"));
+        }
+
+        const user = await User.findOne({
+            email:email
+        })
+        .select("_id email +password");
+
+        if(!bcrypt.compareSync(password, user.password)){
+
+            return next(new CustomError(400, "Check your credentials"));
+        }
+
+        sendJwtToCookie(user, res);
+
     }
     catch(err){
         return next(err);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { register, getUser } from "../controllers/auth.js";
+import { register, login, getUser } from "../controllers/auth.js";
 import {getAccessToRoute} from "../middlewares/auth/auth.js";
+import { isUserExists } from "../middlewares/query/query.js";
 
 
 const router = Router();
 
 router.post("/register", register);
-router.post("/getUser", getAccessToRoute, getUser);
+router.post("/login", isUserExists, login);
+router.get("/getUser", getAccessToRoute, getUser);
 
 export default router;
