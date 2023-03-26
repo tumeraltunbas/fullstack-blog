a75e08d43d2fb7b26527b563a5f7e5198074d919
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-03-26 22:05:26 +0300

    Add getPostOwnerAccess middleware

--- backend/middlewares/auth/auth.js
+++ backend/middlewares/auth/auth.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import { Post } from "../../models/Post.js";
+import CustomError from "../../utils/error/CustomError.js";
 
 
 export const getAccessToRoute = (req, res, next) => {
@@ -14,4 +16,29 @@ export const getAccessToRoute = (req, res, next) => {
         req.user = decoded;
         next()
     })
+}
+
+export const getPostOwnerAccess = async(req, res, next) => {
+    try{
+        const {id} = req.params;
+
+        const post = await Post.findOne({
+            _id:id,
+            isVisible:true       
+        })
+        .select("_id user");
+
+        if(!post){
+            return next(new CustomError(400, "There is no post with that id"));
+        }
+
+        if(post.user != req.user.id){
+            return next(new CustomError(403, "You can not access this route because you are not owner of this post"));
+        }
+
+        next();
+    }
+    catch(err){
+        return next(err);
+    }
 }
